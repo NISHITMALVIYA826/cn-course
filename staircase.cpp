62784ce070f4d97d57182d6a5fd65d23b14ba1a2
nehaaggarwal6871 <43915034+nehaaggarwal6871@users.noreply.github.com>
2022-10-26 17:15:07 +0530

    Update staircase.cpp

--- staircase.cpp
+++ staircase.cpp
@@ -1,29 +1,55 @@
+
+/* 
+
+A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3 steps at a time. Implement a method to count how many possible ways the child can run up the stairs.
+
+Examples: 
+
+Input : 4
+Output : 7
+Explanation:
+Below are the seven ways
+ 1 step + 1 step + 1 step + 1 step
+ 1 step + 2 step + 1 step
+ 2 step + 1 step + 1 step 
+ 1 step + 1 step + 2 step
+ 2 step + 2 step
+ 3 step + 1 step
+ 1 step + 3 step
+
+Input : 3
+Output : 4
+Explanation:
+Below are the four ways
+ 1 step + 1 step + 1 step
+ 1 step + 2 step
+ 2 step + 1 step
+ 3 step
+
+*/
+
+// A C++ program to count number of ways
+// to reach n't stair when
 #include <iostream>
 using namespace std;
- 
-class Staircase{
- 
-    // Returns count of ways to reach n-th stair
-    // using 1 or 2 or 3 steps.
-public:
-    int findStep(int n)
-    {
-        if (n == 0)
-            return 1;
-        else if (n < 0)
-            return 0;
- 
-        else
-            return findStep(n - 3) + findStep(n - 2)
-                   + findStep(n - 1);
-    }
-};
- 
-// Driver code
+
+// A recursive function used by countWays
+int countWays(int n)
+{
+	int res[n + 1];
+	res[0] = 1;
+	res[1] = 1;
+	res[2] = 2;
+	for (int i = 3; i <= n; i++)
+		res[i] = res[i - 1] + res[i - 2] + res[i - 3];
+
+	return res[n];
+}
+
+// Driver program to test above functions
 int main()
 {
-    Staircase s;
-    int n = 4;
-    cout << s.findStep(n);
-    return 0;
+	int n = 4;
+	cout << countWays(n);
+	return 0;
 }
